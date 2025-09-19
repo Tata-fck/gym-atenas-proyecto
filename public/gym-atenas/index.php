4f46b0d44948b3830d1027dbcc9bc515d057c03c
Tata52 <alexcopca03@gmail.com>
2025-09-19 09:37:58 -0600

    pagina principal funcional v1

--- public/gym-atenas/index.php
+++ public/gym-atenas/index.php
@@ -20,10 +20,76 @@ include(SHARED_PATH . "/elementos/head-vcard/head.php");
             </div>
         </div>
         <div class="contenedor-2">
-            
+            <div class="adornos">
+                <img class="adorno-1" src="img/flecha-r.svg" alt="Adornos">
+                <img class="adorno-2" src="img/flecha-l.svg" alt="Adornos">
+            </div>
+            <div class="adornos-2">
+                <img class="adorno-1" src="img/flecha-r.svg" alt="Adornos">
+                <img class="adorno-2" src="img/flecha-l.svg" alt="Adornos">
+            </div>
+            <div class="tarjeta">
+                <h2>SERVICIOS</h2>
+                <ul>
+                    <li>Entrenadores</li>
+                    <li>Capacitados</li>
+                    <li>Regaderas</li>
+                    <li>Vestidores</li>
+                    <li>Estacionamiento </li>
+                    <li>WiFi</li>
+                </ul>
+            </div>
+            <div class="tarjeta">
+                <h2>PRODUCTOS</h2>
+                <ul>
+                    <li>Bebidas energéticas</li>
+                    <li>Agua</li>
+                    <li>Snacks</li>
+                    <li>Barras de Avena</li>
+                </ul>
+            </div>
+            <div class="tarjeta">
+                <h2>ACTIVIDADES</h2>
+                <ul>
+                    <li>Taekwondo</li>
+                    <li>Cardio Escaladoras </li>
+                    <li>Escaladoras</li>
+                    <li>Jumping</li>
+                    <li>Zumba</li>
+                </ul>
+            </div>
+            <div class="tarjeta">
+                <h2>HORARIO</h2>
+                <ul>
+                    <li>LUNES A VIERNES: 6 AM - 11 PM</li>
+                    <li>SÁBADO Y DOMINGO: 8 AM - 6 PM </li>
+                </ul>
+            </div>
+            <div class="direccion">
+                <img src="img/ubicacion-icon.svg" alt="Ubicación">
+                <div class="info-direccion">
+                    <h2>DIRECCIÓN</h2>
+                    <p>C. Gitana, La Nopalera, Tláhuac, 13220 Ciudad de México, CDMX.</p>
+                </div>
+            </div>
+            <div class="contacto">
+                <div class="titulo-contacto">
+                    <h2>CONTÁCTANOS:</h2>
+                </div>
+                <div class="info-contact">
+                    <div class="contacto-linea">
+                        <img src="img/phone-icon.svg" alt="Icono de Teléfono">
+                        <p>55 1234 5678</p>
+                    </div>
+                    <div class="contacto-linea">
+                        <img src="img/web-icon.svg" alt="Icono de Web">
+                        <p>www.nexocard.tech/gym-atenas</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </main>
-    
+
     <?php include("elementos/footer.html");?>
     <link rel="stylesheet" href="elementos/footer.css">
 
