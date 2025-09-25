ec195323c1f4f0f16520887d99e6045497be4a31
brandon-informatico <brandon.ortiz.informatico@gmail.com>
2025-09-25 12:56:25 -0600

    cambio suplementos

---
+++ public/gym-atenas/suplementos.php
@@ -0,0 +1,41 @@
+<?php
+$page_title = "Gym Atenas Industria";
+
+require_once __DIR__ . '../../../bootstrap.php';
+
+include(SHARED_PATH . "/elementos/head-vcard/head.php");
+?>
+
+
+<body>
+    <main>
+        <?php include("elementos/header.html");?>
+        <link rel="stylesheet" href="elementos/header.css">
+
+        <link rel="stylesheet" href="css/suplementos.css">
+
+        <div class="contenedor-1">
+            <div class="separador"></div>   
+            <div class="seccion-suplementos">
+                <h1>SUPLEMENTOS & MÁS</h1>
+            </div>
+            <div class="seccion-texto-1">
+                <p>Complementa tu entrenamiento con productos seleccionados para potenciar tu rendimiento: </p>
+            </div>
+            <div class="seccion-contenido">
+                <h2><span>PROTEINAS</span></h2>
+                <h2><span>CREATINA</span></h2>
+                <h2><span>PREENTRENOS</span></h2>
+                <h2><span>BEBIDAS ENERGÉTICAS</span></h2>
+                <h2><span>SNACKS SALUDABLES</span></h2>
+            </div>
+            <div class="seccion-texto-2">
+                <p>Todo pensado para mantener tu energía y apoyar tus objetivos sin complicaciones.</p>             
+            </div>
+    </main>
+
+     <?php include("elementos/footer.html");?>
+    <link rel="stylesheet" href="elementos/footer.css">
+
+    <?php include(SHARED_PATH . "/elementos/footer-vcard/footer.html");?>
+</body>
